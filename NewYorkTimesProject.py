7e0980778054d823b054ac1d196cb66863ea3387
Blaine Love <blainelove94@gmail.com>
2022-05-11 10:46:12 -0400

    class

--- NewYorkTimesProject.py
+++ NewYorkTimesProject.py
@@ -1,18 +1,29 @@
 from datetime import date
+from unicodedata import name
 from bs4 import BeautifulSoup
 from textblob import TextBlob
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import requests 
 from fastapi import FastAPI
+from pydantic import BaseModel
+import itertools
+import gc
 
 
-app = FastAPI()
-
-@app.get("/")
-def index():
-    return {"name": "First Data"}
 
 
+class Title():
+    
+    def __init__(self,id, title, polarity,subjectivity):
+        self.id = id
+        self.title = title
+        self.polarity = polarity
+        self.subjectivity = subjectivity
+
+    def __str__(self):
+        return  "\nTitle: " + self.title + "\npolarity: " + str(self.polarity) + "\nsubjectitvity: " + str(self.subjectivity)
+   
+    
 
 
 source = requests.get("https://www.nytimes.com/").text
@@ -28,19 +39,35 @@ for h3 in content.find_all('h3', class_="indicate-hover css-1pvrrwb"):
     title = h3.text
     title = title.split()
     titles.append(title)
+
 polara = []
 subje = []
 for counts, title in enumerate(titles):
     title = " ".join(title)
-    print(title)
+    titles[counts] = title
 
     analysis = TextBlob(title)
-    print(analysis.sentiment)
     polara.append(analysis.sentiment.polarity)
     subje.append(analysis.sentiment.subjectivity)
+
+call = []
+for i in range(len(titles)):
+    titlei = Title(i,titles[i],polara[i],subje[i])
+    call.append(titlei)
+
+print(call[0])
+dictionar={
+    
+    1: {
+        call
+    }
+    
+}
 addedPol = 0
 addedSub = 0
 amount = len(polara)
+
+
 for pol in polara:
     addedPol +=pol
 totalPol = addedPol/amount
@@ -55,17 +82,17 @@ if totalSub <= 0.5:
 if totalSub > 0.5:
     print("The title is on average more subjective with average score of", totalSub)
 
-# for i in answer:
-#     print(i)
-#     titles[counts] = title
-# for title in titles:
-#     print(title)
 
-# mainTitle = content.find("h3", class_ = "indicate-hover css-vip0cf")
-# print(mainTitle.text)
 
 app = FastAPI()
 
 @app.get("/")
 def index():
     return {"name": "First Data"}
+
+@app.get("/get-list/{list_id}")
+def get_list(list_id: int):
+    return(dictionar[list_id])
+    
+
+
